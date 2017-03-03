14cd7b50580a5f8bb9a3937ec37a981ba52cf9b8
MrLYC <imyikong@gmail.com>
2017-03-03 10:47:57 +0800

    update get_uri

--- mock_server/utils.py
+++ mock_server/utils.py
@@ -3,17 +3,25 @@
 import hashlib
 
 
+def get_uri_item(key, value, strict=False, encoding="utf-8"):
+    if not isinstance(value, bytes):
+        value = value.encode(encoding)
+
+    if len(value) >= 16 and not strict:  # choice a shortest result
+        value = hashlib.md5(value).hexdigest()
+    else:
+        value = "#%s" % "".join("%x" % i for i in value)
+
+    return "%s=%s" % (key, value)
+
+
 def get_uri(schema, items, strict=False, encoding="utf-8"):
     paths = []
     for k, v in items.items():
-        if not isinstance(v, bytes):
-            v = v.encode(encoding)
+        paths.append(get_uri_item(k, v, strict, encoding))
 
-        if len(v) >= 16 and not strict:
-            v = hashlib.md5(v).hexdigest()
-        else:
-            v = "#%s" % "".join("%x" % i for i in v)
+    path = "/".join(paths)
+    if len(path) > 255 and not strict:  # redis max key size
+        path = get_uri_item("!", path, strict=False)
 
-        paths.append("%s=%s" % (k, v))
-
-    return "%s:%s" % (schema, "/".join(paths))
+    return "%s:%s" % (schema, path)
