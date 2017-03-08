da1fc602aef51115fb23890c3414bc121770243b
MrLYC <imyikong@gmail.com>
2017-03-08 14:18:13 +0800

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,109 +1,47 @@
 (function() {
-    var FieldType = {
-        string: "string",
-        integer: "integer",
-        boolean: "boolean",
-        choice: "choice",
-        extend: "extend",
-        object: "object",
-    };
- 
-    function Proxy(object, attributes) {
+    function Request (success_callback, error_callback) {
         var self = this;
-        self.__proto__ = object;
-        for(k in attributes) {
-            self[k] = attributes[k];
+        self.success_callback = success_callback;
+        self.error_callback = error_callback;
+        self.request = new XMLHttpRequest();
+        self.request.onload = function (){
+            if (success_callback && self.status >= 200 && self.status < 400) {
+                success_callback(self);
+            }
+            else if (error_callback) {
+                error_callback(self);
+            }
+        }
+        self.request.onerror = function () {
+            if (error_callback) {
+                error_callback(self);
+            }
+        }
+
+        self.postJson = function (url, data) {
+            var request = self.request;
+            request.open("POST", url, true);
+            request.setRequestHeader("Content-Type", "application/json");
+            request.send(JSON.stringify(data));
+        }
+
+        self.getJson = function (url) {
+            var request = self.request;
+            request.open("GET", url, true);
+            request.send();
         }
     }
 
-    function Field(name, type, default_value, choices) {
+    function SchemaAPI () {
         var self = this;
-        self.name = name;
-        self.type = type;
-        self.default_value = default_value;
-        self.choices = choices;
-    }
-
-    function ItemValue(field, value) {
-        var self = this;
-        self.field = field;
-        self.value = value;
-    }
-
-    function UriItem(uri, request, response) {
-        var self = this;
-        self.uri = uri;
-        self.request = request;
-        self.response = response;
-    }
-
-    function Schema(name, schema, request_fields, response_fields) {
-        var self = this;
-        self.name = name;
-        self.schema = schema;
-        self.request_fields = request_fields;
-        self.response_fields = response_fields;
-    }
-
-    var schemas = {
-        HTTP: new Schema(
-            "HTTP", "mock_http",
-            [
-                new Field("method", FieldType.choice, "GET", [
-                    "GET", "POST", "PUT", "DELETE", "HEAD", "PATCH",
-                ]),
-                new Field("path", FieldType.string),
-                new Field("query_string", FieldType.extend),
-                new Field("http_header", FieldType.extend),
-            ],
-            [
-                new Field("status_code", FieldType.integer, 200),
-                new Field("status_reason", FieldType.string, "ok"),
-                new Field("cookie", FieldType.object),
-                new Field("header", FieldType.object),
-                new Field("data_type", FieldType.choice, "raw", [
-                    "static_file", "base64", "raw",
-                ]),
-            ]
-        ),
-        TCP: new Schema(
-            "TCP", "mock_tcp",
-            [
-                new Field("request", FieldType.string),
-            ],
-            [
-                new Field("greeting", FieldType.string),
-                new Field("sep_regex", FieldType.string),
-                new Field("data_type", FieldType.choice, "raw", [
-                    "base64", "raw",
-                ]),
-                new Field("data", FieldType.string),
-                new Field("close_stream", FieldType.boolean, false, [
-                    true, false,
-                ]),
-            ]
-        ),
-    }
-    
-    vm = new Vue({
-        el: "#main",
-        data: {
-            schemas: schemas,
-            item: new UriItem(),
-            x_schema: "HTTP",
-            x_items: [],
-        },
-        computed: {
-            schema: {
-                get: function() {
-                    return this.schemas[this.x_schema];
-                },
-                set: function(schema) {
-                    this.x_schema = schema.name;
-                    this.x_items = [];
-                },
-            },
-            
+        self.url = "/schemas";
+        self.get = function (schemas, success_callback, error_callback) {
+            var query_string = []
+            for (schema of schemas) {
+                query_string.push("schema=" + schema);
+            }
+            var request = new Request(success_callback, error_callback);
+            request.getJson(self.url + "?" + encodeURI(query_string.join("&")));
         }
-    });
+    }
 })();
