892d7ac6e3a6f191c9534ab36ba9fee47055187c
MrLYC <imyikong@gmail.com>
2017-03-07 20:24:53 -0800

    update parse_uri

--- mock_server/utils.py
+++ mock_server/utils.py
@@ -46,17 +46,48 @@ def parse_uri_item(value, strict=False):
 
 
 def parse_uri(uri):
-    schema, _, paths = uri.partition(":")
+    from pyparsing import (
+        hexnums, nums, alphanums, Word, Literal,
+        Optional, Forward, ZeroOrMore, Or, ParseException,
+    )
+
+    schema = Word(alphanums + "_-")
+    schema_declare_char = Literal(":")
+    key_declare_char = Literal("=")
+    value = Word(nums + hexnums)
+    value_type = Optional(Word("~-._", max=1), default="")
+    value_item = (value_type + value)
+    key = Word(alphanums + "_-")
+    hash_key = Word("!")
+    item_sep = Literal("/")
+    optional_item_sep = Optional(item_sep, default="/")
+    item = Forward()
+    item << ZeroOrMore(
+        (key + key_declare_char + value_item + optional_item_sep) |
+        (item_sep + item)
+    )
+    hash_item = hash_key + key_declare_char + value_item + optional_item_sep
+    uri_parser = schema + schema_declare_char + Or(hash_item | item)
+
+    try:
+        uri_tokens = list(uri_parser.parseString(uri))
+    except ParseException as err:
+        raise ValueError(str(err))
+
+    i_tokens = iter(uri_tokens)
+    schema = next(i_tokens)
     strict = True
     fields = []
     items = {}
-    for i in paths.split("/"):
-        key, _, value = i.partition("=")
-        if strict:
-            if key == "!" or value.startswith("~"):
-                strict = False
-        fields.append(key)
-        items[key] = value
+    next(i_tokens)
+    for k, _, t, v, _ in zip(
+        i_tokens, i_tokens, i_tokens, i_tokens, i_tokens,
+    ):
+        fields.append(k)
+        if k == "!" or t != "":
+            strict = False
+        items[k] = "%s%s" % (t, v)
+
     return UriInfo(
         schema=schema, fields=fields,
         items=items, strict=strict,
